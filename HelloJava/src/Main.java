eb84ffb6b59e756878162336a246c61fecddff52
Karunya <karunyasheba93@gmail.com>
2026-02-03 00:51:46 +0530

    First commit

---
+++ HelloJava/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+           System.out.println("My Name is Machi");
+           System.out.println("I am Learning Java");
+           System.out.println("I will get IT Job");
+    }
+}
